a37d9e506b57fedd8f087a0dbe301a90c7133e19
Maharshi <maharshi@careerninja.in>
2023-07-31 00:52:26 +0530

    Add: Html response for health check route

--- src/config/loaders/expressLoader.js
+++ src/config/loaders/expressLoader.js
@@ -50,7 +50,55 @@ export default ({ app, express }) => {
 
   // Health route
   app.get('/health', (req, res) => {
-    res.status(200).send('Health OK');
+    // res.status(200).send('Health OK');
+    const htmlResponse = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Health Check</title>
+        <style>
+          /* CSS styles here */
+          .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+          }
+
+          .content {
+            text-align: center;
+          }
+
+          .health {
+            font-weight: bold;
+            font-size: 250%;
+            color: black;
+          }
+
+          .ok {
+            font-weight: bold;
+            font-size: 250%;
+            color: green;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="content">
+            <p>
+              <span class="health">Health: </span>
+              <span class="ok">OK</span>
+            </p>
+            <p class="version">Application Version: v1.0.0</p>
+            <p>This is an official response from the GoParts backend service.</p>
+          </div>
+        </div>
+      </body>
+      </html>
+    `;
+
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(htmlResponse);
   });
 
   return app;
